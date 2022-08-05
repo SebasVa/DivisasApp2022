35cf348af289e7c519a19d1eb37a22bda08044af
SebasVA <svalencia310@gmail.com>
2022-08-04 21:07:44 -0500

    Agregar archivos de proyecto.

---
+++ DivisasApp2022/DivisasApp2022/Views/ConversorView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DivisasApp2022.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConversorView : ContentPage
+    {
+        const double tasa = 19.11;
+
+        public ConversorView()
+        {
+            InitializeComponent();
+        }
+
+        void Limpiar()
+        {
+            PesosEntry.Text = string.Empty;
+            DolaresLabel.Text = string.Empty;
+        }
+
+        private async void ConversorButton_Clicked(object sender, EventArgs e)
+        {
+            double pesos = 0, dolares = 0;
+
+            if (double.TryParse(PesosEntry.Text, out pesos))
+            {
+                dolares = pesos / tasa;
+                DolaresLabel.Text = $"{dolares:N2}";
+            }
+            else
+            {
+                await DisplayAlert("Error", "Cantidad no válida", "OK");
+                Limpiar();
+            }
+        }
+
+        private void LimpiarButton_Clicked(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
